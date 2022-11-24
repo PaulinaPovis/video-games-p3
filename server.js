86bec8956a9712333aa9a2d90ed4bd2d99f2364a
cbanciu1603 <cbanciu@uoc.edu>
2022-11-24 22:56:51 +0100

    implementación PUG front-end y eliminación de los archivos html que ya no se utilizan

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
-app.use( express.static('public') );
+app.use( express.static('public'));
 
 // routers
 app.use('/api',require('./src/route/UserRouters'));
