476e3a112ace077f535b6a0057a99b3351170924
cbanciu1603 <cbanciu@uoc.edu>
2022-11-30 22:16:34 +0100

    logica del juego y conexión con el backend

---
+++ public/js/board-positions.js
@@ -0,0 +1,291 @@
+export const positions = [
+    {
+        id: 1,
+        color: "NONE",
+        positionX: 1,
+        positionY: 1,
+        status: false,
+        options: [2, 7, 8]
+    },
+    {
+        id: 2,
+        color: "NONE",
+        positionX: 2,
+        positionY: 1,
+        status: false,
+        options: [1, 3, 7, 8, 9]
+    },
+    {
+        id: 3,
+        color: "NONE",
+        positionX: 3,
+        positionY: 1,
+        status: false,
+        options: [2, 4, 8, 9, 10]
+    },
+    {
+        id: 4,
+        color: "NONE",
+        positionX: 4,
+        positionY: 1,
+        status: false,
+        options: [3, 5, 9, 10, 11]
+    },
+    {
+        id: 5,
+        color: "NONE",
+        positionX: 5,
+        positionY: 1,
+        status: false,
+        options: [4, 6, 10, 11, 12]
+    },
+    {
+        id: 6,
+        color: "NONE",
+        positionX: 6,
+        positionY: 1,
+        status: false,
+        options: [5, 11, 12]
+    },
+    {
+        id: 7,
+        color: "NONE",
+        positionX: 1,
+        positionY: 2,
+        status: false,
+        options: [1, 2, 8, 14, 13]
+    },
+    {
+        id: 8,
+        color: "NONE",
+        positionX: 2,
+        positionY: 2,
+        status: false,
+        options: [1, 2, 3, 7, 9, 13, 14, 15]
+    },
+    {
+        id: 9,
+        color: "NONE",
+        positionX: 3,
+        positionY: 2,
+        status: false,
+        options: [2, 3, 4, 8, 10, 14, 15, 16]
+    },
+    {
+        id: 10,
+        color: "NONE",
+        positionX: 4,
+        positionY: 2,
+        status: false,
+        options: [3, 4, 5, 9, 11, 15, 16, 17]
+    },
+    {
+        id: 11,
+        color: "NONE",
+        positionX: 5,
+        positionY: 2,
+        status: false,
+        options: [4, 5, 6, 10, 12, 16, 17, 18]
+    },
+    {
+        id: 12,
+        color: "NONE",
+        positionX: 6,
+        positionY: 2,
+        status: false,
+        options: [5, 6, 11, 17, 18]
+    },
+    {
+        id: 13,
+        color: "NONE",
+        positionX: 1,
+        positionY: 3,
+        status: false,
+        options: [7, 8, 14, 19, 20]
+    },
+    {
+        id: 14,
+        color: "NONE",
+        positionX: 2,
+        positionY: 3,
+        status: false,
+        options: [7, 8, 9, 13, 15, 19, 20, 21]
+    },
+    {
+        id: 15,
+        color: "NONE",
+        positionX: 3,
+        positionY: 3,
+        status: false,
+        options: [8, 9, 10, 14, 16, 20, 21, 22]
+    },
+    {
+        id: 16,
+        color: "NONE",
+        positionX: 4,
+        positionY: 3,
+        status: false,
+        options: [9, 10, 11, 15, 17, 21, 22, 23]
+    },
+    {
+        id: 17,
+        color: "NONE",
+        positionX: 5,
+        positionY: 3,
+        status: false,
+        options: [10, 11, 12, 16, 18, 22, 23, 24]
+    },
+    {
+        id: 18,
+        color: "NONE",
+        positionX: 6,
+        positionY: 3,
+        status: false,
+        options: [11, 12, 17, 23, 24]
+    },
+    {
+        id: 19,
+        color: "NONE",
+        positionX: 1,
+        positionY: 4,
+        status: false,
+        options: [13, 14, 20, 25, 26]
+    },
+    {
+        id: 20,
+        color: "NONE",
+        positionX: 2,
+        positionY: 4,
+        status: false,
+        options: [13, 14, 15, 19, 21, 25, 26, 27]
+    },
+    {
+        id: 21,
+        color: "NONE",
+        positionX: 3,
+        positionY: 4,
+        status: false,
+        options: [14, 15, 16, 20, 22, 26, 27, 28]
+    },
+    {
+        id: 22,
+        color: "NONE",
+        positionX: 4,
+        positionY: 4,
+        status: false,
+        options: [15, 16, 17, 21, 23, 27, 28, 29]
+    },
+    {
+        id: 23,
+        color: "NONE",
+        positionX: 5,
+        positionY: 4,
+        status: false,
+        options: [16, 17, 18, 22, 24, 28, 29, 30]
+    },
+    {
+        id: 24,
+        color: "NONE",
+        positionX: 6,
+        positionY: 4,
+        status: false,
+        options: [17, 18, 23, 29, 30]
+    },
+    {
+        id: 25,
+        color: "NONE",
+        positionX: 1,
+        positionY: 5,
+        status: false,
+        options: [19, 20, 26, 31, 32]
+    },
+    {
+        id: 26,
+        color: "NONE",
+        positionX: 2,
+        positionY: 5,
+        status: false,
+        options: [19, 20, 21, 25, 27, 31, 32, 33]
+    },
+    {
+        id: 27,
+        color: "NONE",
+        positionX: 3,
+        positionY: 5,
+        status: false,
+        options: [20, 21, 22, 26, 28, 32, 33, 34]
+    },
+    {
+        id: 28,
+        color: "NONE",
+        positionX: 4,
+        positionY: 5,
+        status: false,
+        options: [21, 22, 23, 27, 29, 33, 34, 35]
+    },
+    {
+        id: 29,
+        color: "NONE",
+        positionX: 5,
+        positionY: 5,
+        status: false,
+        options: [22, 23, 24, 28, 30, 34, 35, 36]
+    },
+    {
+        id: 30,
+        color: "NONE",
+        positionX: 6,
+        positionY: 5,
+        status: false,
+        options: [23, 24, 29, 35, 36]
+    },
+    {
+        id: 31,
+        color: "NONE",
+        positionX: 1,
+        positionY: 6,
+        status: false,
+        options: [25, 26, 32]
+    },
+    {
+        id: 32,
+        color: "NONE",
+        positionX: 2,
+        positionY: 6,
+        status: false,
+        options: [25, 26, 27, 31, 33]
+    },
+    {
+        id: 33,
+        color: "NONE",
+        positionX: 3,
+        positionY: 6,
+        status: false,
+        options: [26, 27, 28, 32, 34]
+    },
+    {
+        id: 34,
+        color: "NONE",
+        positionX: 4,
+        positionY: 6,
+        status: false,
+        options: [27, 28, 29, 33, 35]
+    },
+    {
+        id: 35,
+        color: "NONE",
+        positionX: 5,
+        positionY: 6,
+        status: false,
+        options: [28, 29, 30, 34, 36]
+    },
+    {
+        id: 36,
+        color: "NONE",
+        positionX: 6,
+        positionY: 6,
+        status: false,
+        options: [29, 30, 35]
+    }
+
+]
--- public/js/game.js
+++ public/js/game.js
@@ -1,9 +1,133 @@
+import positionsJson from '../board-position.json' assert {type: 'json'};
+import { WinStorage } from "./classes/WindowStorageManager.js";
+
+// Variables necesarias y nodos del DOM
+const user = WinStorage.getParsed('currentUser');
+const roomSelected = WinStorage.getParsed('roomSelected');
 const squares = document.querySelectorAll('.board-item');
+let player = undefined;
+let gameId = undefined;
+let hasTwoPlayers = false;
+let lastBoardPosition = undefined;
 
+// Acción para crear la partida llamando al servicio
+function createGame(){
+    //Datos para pasar al backend
+    const data = {
+        player: {
+            id: user.id,
+            userName: user.userName,
+            email: user.email
+        },
+        room: {
+            id: roomSelected.id,
+            name: roomSelected.name
+        }
+    };
 
-squares.forEach((item) => {
-    item.addEventListener('click', function(event){
-        console.log(event.target)
-        console.log(event.target.id)
+    fetch('http://localhost:3000/api/games', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: new Headers(
+            {
+                'Content-Type':  'application/json'
+            }
+        )          
     })
-})
+    .then(data => data.json()) 
+    .then(response => {
+        /**
+         * 1 - Si va bien, asignamos valor a la variable player con el jugador que nos devuelve el
+         * servicio con el mismo id
+         * 
+         * 2 - Iniciamos el juego con la función startGame TODO: Aquí deberíamos poder saber si el juego
+         * ya está listo para poder jugar (2 jugadores), ahora mismo no sé si se puede saber
+         */
+
+        player = response.players.find(item => item.id === user.id);
+        gameId = response.id;
+        console.log(player);
+        startGame();
+    })
+    .catch((err) => {
+        //Si va mal
+        console.log(err)
+    })
+};
+
+//Invocamos la función nada más iniciar la vista para que cree la partida
+createGame();
+
+
+//Acción para comenzar el juego
+function startGame(){
+    //Recorremos el array de cuadrados del HTML
+    squares.forEach((element) => {
+        //Asignamos un escuchador con el evento click a cada elemento del array
+        element.addEventListener('click', function(event){
+            
+            const selected = Number(event.target.id);
+            const boardPosition = positionsJson.positions.find(item => item.id === selected)
+    
+            console.log("Board position es: ", boardPosition);
+            console.log(event.target.id)
+    
+            if(boardPosition.status === false && lastBoardPosition === undefined) {
+                // Fetch al backend
+                fetchCell(selected);
+    
+                boardPosition.status = true;
+                lastBoardPosition = boardPosition;
+    
+                console.log("Fuera del else Last position es: ", lastBoardPosition);
+            } 
+            else {
+    
+                if(lastBoardPosition.options.includes(selected) && boardPosition.status === false) {
+                    // Fetch al backend
+                    fetchCell(selected);
+    
+                    boardPosition.status = true;
+                    lastBoardPosition = boardPosition;
+                    console.log("Last position es: ", lastBoardPosition);
+                } 
+                else {
+                    console.log("No entra")
+                }
+            }
+        })
+    });
+}
+
+
+// Acción para llamar al backend con el número de celda seleccionada
+function fetchCell(cellNumber){
+    //Datos a enviar al backend
+    const data = {
+        id: player.id,
+        color: player.color
+    }
+    fetch(`http://localhost:3000/api/games/${gameId}/cells/${cellNumber}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: new Headers(
+            {
+                'Content-Type':  'application/json'
+            }
+        )          
+    })
+    .then(data => data.json()) 
+    .then(response => {
+        
+        if(response.msg === "Error: The cell id 9 can't be register, because not exist 2 players"){
+            //Todavía no hay dos jugadores
+        }
+        else{
+            //Ya hay dos jugadores
+            hasTwoPlayers = true;
+        }
+    }) 
+    .catch((err) => {
+        console.log(err)
+    }) 
+}
